c6b82e2b45705387af02a516e0c6c91ade8d0db8
Guillermo Fuentes <guille3102010@hotmail.com>
2023-05-08 17:34:57 -0300

    first commit

---
+++ git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Hello World!"
+# echo is similar to print statement.
+git config --global user.name 'Guillermo Fuentes'
+git config --global user.email 'guille3102010@hotmail.com'
+git config --global color.ui true
+git config --global pull.rebase true
+git config --global credential.username "Guillehfg"
+# git branch --set-upstream-to=origin/master master
+git config --global alias.lodg 'log --oneline --decorate --graph'
+git config --global alias.conflict 'diff --name-only --diff-filter=U'
+# git config --global --get-regexp alias
+git config --global --unset alias.lodg
+git config --global --list
+# git config --global --unset user.email
+read -p "Accepting the offer? (y/n) " answer
+
